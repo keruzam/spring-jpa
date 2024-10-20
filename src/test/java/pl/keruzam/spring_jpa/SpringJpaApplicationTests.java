495e61b6c174cc115df45ddc0ae3541212c5dfea
keruzam <mazurek.zg@gmail.com>
2024-10-20 16:47:06 +0200

    inti project

---
+++ src/test/java/pl/keruzam/spring_jpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.keruzam.spring_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
